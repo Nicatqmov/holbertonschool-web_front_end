8ad9600c7da6c11c4f1b5e226d2e92a23811e394
GasimovNijat <nijatt.gasimov@gmail.com>
2024-03-31 23:40:25 +0400

    boostrap

---
+++ Javascript_advanced/0-welcome.js
@@ -0,0 +1,8 @@
+function welcome(firstName,lastName ){
+    let fullName = firstName + ' ' +lastName;
+    displayFullName(fullName)
+}
+
+function displayFullName(fullName ){
+    alert('Welcome '+fullName+'!') 
+}
